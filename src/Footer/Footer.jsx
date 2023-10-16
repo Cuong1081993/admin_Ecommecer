7cb1c9259c1d0ed4afec1b0fb5cf3ad211c720f3
unknown <ntqcuong1993@gmail.com>
2023-10-16 23:18:47 +0700

    admin

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="footer text-center text-muted">
+      All Rights Reserved by
+      <a href="https://www.facebook.com/frozen1008"> Nguyễn Trí Quốc Cường</a>.
+    </footer>
+  );
+};
+
+export default Footer;
